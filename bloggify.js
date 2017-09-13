d59648beca537b10568437b97ec04c893bbf27fc
Ionică Bizău <bizauionica@gmail.com>
2017-09-13 18:27:55 +0300

    2.0.0

--- bloggify.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-module.exports = require("bloggify-config")(require("./config"));
--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = (lien, cb) => {
-    cb(null, {
-        title: "Home"
-    });
-};
